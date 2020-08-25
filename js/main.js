702b50ed72997466a462781a02c0d445c1fabba2
Marcleese1 <34348341+Marcleese1@users.noreply.github.com>
2020-08-26 00:37:05 +0100

    responsive done

--- js/main.js
+++ js/main.js
@@ -24,4 +24,43 @@ $(document).ready(function () {
         type: 'image',
         gallery: { enabled: true }
     });
+
+
+    //Sticky Navbar
+
+    let nav_offset_top = $(".header-area").height()+50;
+    function navbarFixed(){
+        if($(".header-area").length){
+           $(window).scroll(function(){
+               let scroll =$(window).scrollTop();
+               if(scroll >= nav_offset_top){
+                $('.header-area .main-menu').addClass('navbar_fixed');
+               }else{
+                $('.header-area .main-menu').removeClass('navbar_fixed');
+               }
+           }) 
+        }
+    }
+
+    navbarFixed();
+
+    $(document).on('click', 'a[href^="#"]', function(e) {
+        // target element id
+        var id = $(this).attr('href');
+    
+        // target element
+        var $id = $(id);
+        if ($id.length === 0) {
+            return;
+        }
+    
+        // prevent standard hash navigation (avoid blinking in IE)
+        e.preventDefault();
+    
+        // top position relative to the document
+        var pos = $id.offset().top;
+    
+        // animated top scrolling
+        $('body, html').animate({scrollTop: pos});
+    });
 });
